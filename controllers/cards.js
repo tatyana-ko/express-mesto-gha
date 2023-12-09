60a743e83de403ad95cd505befeca0ba0f579c58
Tatyana Koba <misskotanyaz@gmail.com>
2023-12-09 14:13:59 +0300

    add

--- controllers/cards.js
+++ controllers/cards.js
@@ -53,6 +53,11 @@ module.exports.deleteCard = async (req, res) => {
     }
     return res.status(HTTP_STATUS_OK).send({ message: "Карточка успешно удалена"});
   } catch (error) {
+    if(error.name === "CastError") {
+      return res
+        .status(HTTP_STATUS_BAD_REQUEST)
+        .send({ message: "Переданы невалидные данные" });
+    }
     return res
       .status(HTTP_STATUS_INTERNAL_SERVER_ERROR)
       .send({ message: "Ошибка на стороне сервера" });
